7fa69a4e9f2bc8fbb1defeed6e5aa6f603f0ebf9
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-07-19 04:16:45 +0000

    [Add] defaultState

--- reversi/main.js
+++ reversi/main.js
@@ -6,6 +6,19 @@ const createSquares = () => {
     const square = squareTemplate.cloneNode(true); //テンプレートから要素をクローン
     square.removeAttribute("id"); //テンプレート用のid属性を削除
     stage.appendChild(square); //マス目のHTML要素を盤に追加
+
+    const stone = square.querySelector('.stone');
+
+    let defaultState; //盤上各マスの石の状態定義
+    // iの値によって石の状態を分岐
+    if (i == 27 || i == 36) {
+      defaultState = 1;
+    }else if(i == 28 || i ==35){
+      defaultState = 2;
+    }else{
+      defaultState = 0;
+    }
+    stone.setAttribute("data-state", defaultState);
   }
 };
 
